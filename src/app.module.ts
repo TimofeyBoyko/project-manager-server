3ee691ba4e0efcfe6fde84549dfd08c0a1e77754
TimofeyBoyko <fognir742@gmail.com>
2022-10-18 23:46:19 +0500

    apply confif alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import config from './configs/config';
+import config from '@configs/config';
 
 @Module({
   imports: [

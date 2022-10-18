953a296d2dd227ea2a7a356d85e5e5db93f62fb2
TimofeyBoyko <fognir742@gmail.com>
2022-10-18 23:36:18 +0500

    add config service globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const PORT = await configService.get('PORT');
+
+  await app.listen(PORT || 3000);
+
+  console.log(`Server start at port: ${PORT}...`);
 }
 bootstrap();

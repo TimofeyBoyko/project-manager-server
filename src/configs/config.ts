06a85a3ddfa44152be3bc48908ba33b324049c63
TimofeyBoyko <fognir742@gmail.com>
2022-10-19 00:03:53 +0500

    add config for database

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,3 +1,10 @@
 export default () => ({
   PORT: parseInt(process.env.PORT, 10) || 9000,
+  DATABASE: {
+    HOST: process.env.DB_HOST || 'localhost',
+    PORT: parseInt(process.env.DB_PORT, 10) || 3306,
+    USERNAME: process.env.DB_USERNAME || 'root',
+    PASSWORD: process.env.DB_PASSWORD || 'root',
+    DATABASE: process.env.DATABASE || 'project-manager',
+  },
 });
